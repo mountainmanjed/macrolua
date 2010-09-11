a17d87025155719a6c177bc281e3dcb35d051978
dammit9x <dammit9x@hotmail.com>
2010-09-11 01:53:04 +0000

    MacroLua 1.09 updates

--- macro-options.lua
+++ macro-options.lua
@@ -1,30 +1,34 @@
---[[
-This file is intended to be run by macro.lua
-See macro-readme.html for help and instructions.
+--This file is intended to be run by macro.lua
+--See macro-readme.html for help and instructions.
 
-You may adjust these parameters.
-]]--
+--You may adjust these parameters.
+
+--------------------------------------------------------------------------------
+-- File handling settings
 
 -- name of the macro to be played
 playbackfile = "sfz2alj-error1.mis"
 
--- where the macro scripts are stored
+-- where the macro scripts are saved to and loaded from
 path = ".\\macro\\"
 
+--------------------------------------------------------------------------------
+-- Hotkey settings (These only apply if the emulator doesn't have Lua hotkeys.)
+
 -- press to start playing the macro, or to cancel a playing macro
--- only applies if the emulator doesn't have Lua hotkeys
 playkey = "semicolon"
 
 -- press to start and stop recording a macro
--- only applies if the emulator doesn't have Lua hotkeys
-recordkey = "quote"
+recordkey = "numpad/"
+
+-- press to turn on or off whether it should pause after a macro completes
+togglepausekey = "quote"
 
 -- press to convert the playback file to one-line-per-frame format
--- only applies if the emulator doesn't have Lua hotkeys
 inputstreamkey = ""
 
--- pause after a macro completes
-pauseafterplay = false
+--------------------------------------------------------------------------------
+-- Recording file output settings
 
 -- minimum wait frames to be collapsed into Ws when recording
 longwait = 4
@@ -35,8 +39,7 @@ longpress = 10
 -- minimum characters in a line to be broken up when recording
 longline = 60
 
--- run the input display script for FBA-rr or MAME-rr
-showarcadeinput = true
-
+--------------------------------------------------------------------------------
 -- look out for and ignore frameMAME audio commands when parsing
-framemame = false
+-- (This only applies to FBA-rr and MAME-rr.)
+framemame = true
